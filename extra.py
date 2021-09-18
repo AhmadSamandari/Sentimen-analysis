73f9c6092bc07f04fadceb8ce332b0759065c4f8
AhmadSamandari <ahmadsamandari51@gmail.com>
2021-09-18 12:12:45 +0430

    i made some modification

--- extra.py
+++ extra.py
@@ -1,6 +1,20 @@
-def compute(a, b):
-    c = a+b
-    return True
+import nltk
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+#from sklearn import LogisticRegrssion
+from nltk import word_tokenize
+
+#######
+
+stopwords = set([x.rstrip() for x in open('stopwords.txt')])
 
 
-print("t") if compute(10, 15) else print('fuck')
+positive_soup = BeautifulSoup(
+    open('positive.review').read(),  features="lxml")
+positive_soup = positive_soup.findAll('review_text')
+negative_soup = BeautifulSoup(
+    open('negative.review').read(),  features="lxml")
+negative_soup = negative_soup.findAll('review_text')
+
+# print(positive_soup)
